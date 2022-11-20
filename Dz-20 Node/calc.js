c7b14c1f3fcf0d92fa606a594da37940e71fc96e
Dmitriy <50639080+demos1981@users.noreply.github.com>
2022-11-20 23:49:42 +0200

    Add files via upload

---
+++ Dz-20 Node/calc.js
@@ -0,0 +1,8 @@
+const { mult } = require('./calculator');
+const calculator= require('./calculator');// take on export 
+
+console.log(calculator.add(2,3));
+console.log(calculator.sub(20,3));
+console.log(calculator.mult(20,3));
+console.log(calculator.div(21,3));
+
---
+++ Dz-20 Node/calculator/index.js
@@ -0,0 +1,6 @@
+module.exports = { //exports this 
+    add:require('./add'),
+    div:require('./div'),
+    mult:require('./mult'),
+    sub:require('./sub'),
+}
